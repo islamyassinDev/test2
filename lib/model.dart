74ecb859b814d4e5499bdc5a5db1136f5bb99ea0
islamyassinDev <islamyassindev@gmail.com>
2021-03-15 15:14:06 +0200

    FT Commit

---
+++ lib/model.dart
@@ -0,0 +1,52 @@
+class Model {
+  final String firstName;
+  final String lastName;
+  final int age;
+  final String email;
+
+  Model({
+    this.firstName,
+    this.lastName,
+    this.age,
+    this.email,
+  });
+}
+
+List<Model> data = [
+  Model(
+    firstName: 'islam',
+    lastName: 'taha',
+    age: 33,
+    email: 'eng.islamyassin25@gmail.com',
+  ),
+  Model(
+    firstName: 'mohammed',
+    lastName: 'othman',
+    age: 24,
+    email: 'mohammed@gmail.com',
+  ),
+  Model(
+    firstName: 'mahmoud',
+    lastName: 'ali',
+    age: 20,
+    email: 'mahmoud@gmail.com',
+  ),
+  Model(
+    firstName: 'hamed',
+    lastName: 'moustafa',
+    age: 44,
+    email: 'moustafa@gmail.com',
+  ),
+  Model(
+    firstName: 'asmaa',
+    lastName: 'medhat',
+    age: 29,
+    email: 'asmaa@gmail.com',
+  ),
+  Model(
+    firstName: 'kholoud',
+    lastName: 'morsy',
+    age: 37,
+    email: 'kholoud@gmail.com',
+  ),
+];
